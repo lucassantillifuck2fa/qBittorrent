0a82661a23bbb4df8612e96af9a4084c004eee64
lucassantillifuck2fa <ravenholmofficial@gmail.com>
2026-08-21 12:16:17 -0300

    Add files via upload

--- src/gui/statusbar.h
+++ src/gui/statusbar.h
@@ -74,6 +74,8 @@ private:
     QWidget *m_freeDiskSpaceSeparator = nullptr;
     QLabel *m_lastExternalIPsLbl = nullptr;
     QWidget *m_lastExternalIPsSeparator = nullptr;
+    QLabel *m_portLbl = nullptr;
+    QWidget *m_portSeparator = nullptr;
     QLabel *m_DHTLbl = nullptr;
     QWidget *m_DHTSeparator = nullptr;
     QPushButton *m_connecStatusLblIcon = nullptr;
